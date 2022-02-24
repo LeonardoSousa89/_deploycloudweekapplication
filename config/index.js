5dbf91fd2acbfa52cb2d833d8ee3912db214f482
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-02-24 12:13:50 -0300

    master

--- config/index.js
+++ config/index.js
@@ -21,86 +21,28 @@ function dataSend() {
 
         const data = { email, pass }
 
-        let HEADERS = {
-            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin',
-            'Content-Type': 'application/json', //optional
-            'Access-Control-Allow-Methods': 'POST, OPTIONS, GET, PUT, DELETE',
-            'Access-Control-Max-Age': '8640'
-          }
-          
-          //This solves the "No ‘Access-Control-Allow-Origin’ header is present on the requested resource."
-          
-          HEADERS['Access-Control-Allow-Origin'] = '*'
-          HEADERS['Vary'] = 'Origin'
-
         let config = {
             method:'POST',
             body: JSON.stringify(data),
             mode:'cors',
-            HEADERS
-        }
-        
-        exports.handler = async function (event, context) {
-            try {
-              if (event.httpMethod === 'OPTIONS') {
-                return { statusCode: '204', HEADERS }
-              }
-              if (event.httpMethod === 'POST') {
-                  const body = JSON.parse(event.body)
-
-                  fetch(url,config)
-                  .then(response => {
-                      if(response.ok){
-                             storage(data.pass)
-                             document.location.href = urlAppProd
-                      }else{
-                          alert(response.status + ':' + response.statusText)
-                      }
-                  })
-                  .catch((error)=>{
-                      alert(error)
-                  })
-      
-
-                 return {
-                   statusCode: 200,
-                   body: 'Success',
-                   HEADERS
-                 } 
-           
-              }
-              return {
-                statusCode: 401,
-                HEADERS
-              }
-            } catch (e) {
-              console.error(e)
-              return {
-                statusCode: 500,
-                body: e.toString()
-              }
+            headers:{
+                'Content-Type':'application/json'
             }
-          }
-
-        
+        }
+    
+        fetch(url,config)
+                .then(response => {
+                    if(response.ok){
+                           storage(data.pass)
+                           document.location.href = urlAppProd
+                    }else{
+                        alert(response.status + ':' + response.statusText)
+                    }
+                })
+                .catch((error)=>{
+                    alert(error)
+                })
+    
 }
 
-
-
-
-
-/**
- * let HEADERS = {
-  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin',
-  'Content-Type': 'application/json', //optional
-  'Access-Control-Allow-Methods': 'POST, OPTIONS',
-  'Access-Control-Max-Age': '8640'
-}
-
-//This solves the "No ‘Access-Control-Allow-Origin’ header is present on the requested resource."
-
-HEADERS['Access-Control-Allow-Origin'] = '*'
-HEADERS['Vary'] = 'Origin'
- * 
- */
 
